5f8a6f01348fde0ab9d7d4e2493fb3a3e826db87
taylaf001 <fonty005@mymail.unisa.edu.au>
2025-10-05 19:51:07 +1030

    Starting assignment by defining classes

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,18 @@
 """
 File: Hacker.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
+Author: Tayla Fontanabella
+ID: Fonty005
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Hacker:
+    def __init__(self):
+        self.name = "Hackzilla"
+        self.tracelevel = 0
+
+    def inventory(self):
+        cryptotoken = 1
+
+
--- Rig.py
+++ Rig.py
@@ -6,3 +6,24 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Rig(self):
+    def__init__(self):
+        self.name = ""
+        self.damageCounter = 0
+        self.brokenState = False
+        self.upgradeLevel = 0
+    
+    def storage(self):
+        dataSpikes = 2
+        removableDrive = 1
+    
+    def repair(self):
+        if damageCounter > 0:
+            damageCounter = 0
+            self.brokenState = False
+        else:
+            print("No repair is needed.")
+    
+    def upgrade(self):
+        hardwarePatch = 0
