ba35d728763c30bcd44c7f27a4a3514d9e9124f7
taylaf001 <fonty005@mymail.unisa.edu.au>
2025-10-14 21:59:56 +1030

    Methods updated to reflect the use of getters and setters in rig.py
    Slight formatting adjustments made and comments added

--- Rig.py
+++ Rig.py
@@ -9,6 +9,7 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 import random
 from Asset import Asset
+
 # Rig Class
 class Rig:
     def __init__(self):
@@ -60,58 +61,68 @@ class Rig:
         self.__storage = storage
 
     # Rig Upgrade Method
+
     def upgrade(self, asset):
-        if asset.name == "Hardware Patch":
-            self.__storage.remove(asset)
-            self.__upgradeLevel += 1
-            self.__assetStorageAmount += 1
+        if asset.get_name() == "Hardware Patch":
+            self.get_storage().remove(asset)
+            self.set_upgradeLevel(self.get_upgradeLevel() + 1)
+            self.set_assetStorageAmount(self.get_assetStorageAmount() + 1)
+            print(f"{asset.get_name()} upgraded to {self.get_upgradeLevel()}")
 
     # Rig random asset generation method
+
     def assetGeneration(self):
         assetPossibilities = [Asset("Hardware Patch", "Used to upgrade rigs.", False), Asset("Removable Drive", "Used in rigs and used for extraction", False), Asset("Data Spike", "Used in rigs and used for extraction", False), Asset("CryptoToken", "Used to acquire or repair rigs", False), Asset("Security Chip", "Used to encrypt or decrypt assets.", False)]
         randomAsset = random.choice(assetPossibilities)
-        self.__storage.append(randomAsset)
-
+        self.get_storage().append(randomAsset)
 
     # Rig Takes Damage Method
+
     def damageCounter(self):
-        self.__damageCounter += 1
-        if self.__damageCounter == 2 and self.__upgradeLevel == 0:
-            self.__brokenState = True
+        self.set_damageCounter(self.get_damageCounter() + 1)
+        if self.get_damageCounter() == 2 and self.get_upgradeLevel() == 0:
+            self.set_brokenState(True)
 
     # Method to repair rig
+
     def repair(self, asset):
-        if self.__damageCounter > 0 and asset.name == "CryptoToken":
-            self.__damageCounter = 0
-            self.__brokenState = False
-        elif self.__damageCounter > 0 and asset.name != "CryptoToken":
+        if self.get_damageCounter() > 0 and asset.get_name() == "CryptoToken":
+            self.set_damageCounter(0)
+            self.set_brokenState(False)
+        elif self.get_damageCounter() > 0 and asset.get_name() != "CryptoToken":
             print("No valid asset inputted.")
         else:
             print("No repair is needed.")
 
     # Method to store / add asset to rig
+
     def storeAsset(self, asset):
-        self.__storage.append(asset)
+        self.get_storage().append(asset)
 
     # Method releasing asset
+
     def releaseAsset(self, asset):
-        if asset in self.__storage and asset.encrypted == False:
-            self.__storage.remove(asset)
-        elif asset in self.__storage and asset.encrypted == True:
+        if asset in self.get_storage() and asset.get_encrypted() == False:
+            storage = self.get_storage()
+            storage.remove(asset)
+            self.set_storage(storage)
+        elif asset in self.get_storage() and asset.get_encrypted() == True:
             print("Asset must be decrypted before transferring.")
-        elif asset not in self.__storage:
+        elif asset not in self.get_storage():
             print("Asset not found in storage.")
 
     # Method returning rig's condition
-    def condition(self):
-        self.conditionStr = ""
-        if self.__brokenState == False:
-            self.conditionStr = "Pristine"
-        elif self.__brokenState == True:
-            self.conditionStr = "Broken"
 
-        return f"{self.conditionStr} ({self.__upgradeLevel})"
+    def condition(self):
+        conditionStr = ""
+        if self.get_brokenState() == False:
+            conditionStr = "Pristine"
+        elif self.get_brokenState() == True:
+            conditionStr = "Broken"
+
+        return f"{conditionStr} ({self.get_upgradeLevel()})"
 
     # String Conversion Method
-    def __str__(self)
-        return f"{self.__name}, {self.conditionStr}, {self.__upgradeLevel}, {self.__storage}"
+
+    def __str__(self):
+        return f"{self.get_name()}, {conditionStr}, {self.get_upgradeLevel()}, {self.get_storage()}"
